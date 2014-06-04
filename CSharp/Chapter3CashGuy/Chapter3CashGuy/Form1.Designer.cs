a0a8869d6d3556096dd529159000ac80b7207e5c
Alexander Nohe <alex.nohe427@gmail.com>
2014-06-03 18:36:34 -0700

    Chapter Three
    This is work from Chapter 3

---
+++ CSharp/Chapter3CashGuy/Chapter3CashGuy/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Chapter3CashGuy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.joeCashLabel = new System.Windows.Forms.Label();
+            this.bobCashLabel = new System.Windows.Forms.Label();
+            this.bankCashLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // joeCashLabel
+            // 
+            this.joeCashLabel.AutoSize = true;
+            this.joeCashLabel.Location = new System.Drawing.Point(12, 9);
+            this.joeCashLabel.Name = "joeCashLabel";
+            this.joeCashLabel.Size = new System.Drawing.Size(65, 13);
+            this.joeCashLabel.TabIndex = 0;
+            this.joeCashLabel.Text = "Joe has $50";
+            // 
+            // bobCashLabel
+            // 
+            this.bobCashLabel.AutoSize = true;
+            this.bobCashLabel.Location = new System.Drawing.Point(12, 41);
+            this.bobCashLabel.Name = "bobCashLabel";
+            this.bobCashLabel.Size = new System.Drawing.Size(73, 13);
+            this.bobCashLabel.TabIndex = 1;
+            this.bobCashLabel.Text = "Bob has $100";
+            // 
+            // bankCashLabel
+            // 
+            this.bankCashLabel.AutoSize = true;
+            this.bankCashLabel.Location = new System.Drawing.Point(12, 74);
+            this.bankCashLabel.Name = "bankCashLabel";
+            this.bankCashLabel.Size = new System.Drawing.Size(100, 13);
+            this.bankCashLabel.TabIndex = 2;
+            this.bankCashLabel.Text = "The bank has $100";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 182);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(81, 42);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Give $10 to Joe";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(170, 182);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(79, 42);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Recieve $5 From Bob";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.bankCashLabel);
+            this.Controls.Add(this.bobCashLabel);
+            this.Controls.Add(this.joeCashLabel);
+            this.Name = "Form1";
+            this.Text = "Fun With Joe and Bob";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label joeCashLabel;
+        private System.Windows.Forms.Label bobCashLabel;
+        private System.Windows.Forms.Label bankCashLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
