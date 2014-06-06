630094eb887c4737ee13c329fbbe1732fc9e570a
Alexander Nohe <alex.nohe427@gmail.com>
2014-06-05 18:05:59 -0700

    Object References

--- CSharp/Ch4Elephant/Ch4Elephant/Form1.cs
+++ CSharp/Ch4Elephant/Ch4Elephant/Form1.cs
@@ -12,9 +12,31 @@ namespace Ch4Elephant
 {
     public partial class Form1 : Form
     {
+        Elephant lucinda = new Elephant() { Name="Lucinda", EarSize=33 };
+        Elephant lloyd = new Elephant() { Name = "Lloyd", EarSize = 40 };
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lloyd.WhoAmI();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            lucinda.WhoAmI();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Elephant holder;
+            holder = lloyd;
+            lloyd = lucinda;
+            lucinda = holder;
+            MessageBox.Show("Object Swap");
         }
     }
 }
