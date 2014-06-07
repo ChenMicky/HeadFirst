4342bc80b96d4d38555055733d7dc34f6e2445e5
Alexander Nohe <alex.nohe427@gmail.com>
2014-06-06 17:05:27 -0700

    Class

---
+++ CSharp/ChapterFivePartyPlanning/ChapterFivePartyPlanning/DinnerParty.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChapterFivePartyPlanning
+{
+    class DinnerParty
+    {
+        public int NumberOfPeople;
+        public decimal CostOfBeveragesPerPerson, CostOfDecorations;
+        
+        public const int CostOfFoodPerPerson =25;
+
+        public void SetHealthyOption(bool healthyOption)
+        {
+            if(healthyOption)
+            {
+                CostOfBeveragesPerPerson = 5.0M;
+            }
+            else
+            {
+                CostOfBeveragesPerPerson = 20.0M;
+            }
+        }
+
+        public void CalculateCostOfDecorations(bool fancy)
+        {
+            if(fancy)
+            {
+                CostOfDecorations = 15M;
+            }
+            else
+            {
+                CostOfDecorations = 7.50M;
+            }
+        }
+
+        public decimal CalculateCost(bool healthyOption)
+        {
+            decimal totalCost = CostOfDecorations + ((CostOfBeveragesPerPerson + CostOfFoodPerPerson) * NumberOfPeople);
+            if (healthyOption)
+            {
+                return totalCost * .95M;
+            }
+            else
+            {
+                return totalCost;
+            }
+        }
+    }
+}
